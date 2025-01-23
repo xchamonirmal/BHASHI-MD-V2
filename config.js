e8f401e2b3d91f03a8c8fe355fd9bd9fe034a138
xchamonirmal <herokubot458@gmail.com>
2025-01-23 18:45:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "BHASHI-MD~mmQmnbZb#_RJqV3WvHDl6GrFXVVvYvpjsfEEeTWcYeusP2o6m4yc",
+  SESSION_ID: process.env.SESSION_ID || "BHASHI-MD~u3hDDQCR#LElsqMKOgpsfSWR9R8DABoef7Mh4wJ51TH9L57MjJuc",
 // For extra thing : BHASHI-MD~(megafileid)
   MONGODB: process.env.MONGODB || "mongodb+srv://herokubot458:QAT5sKzU9JnlXJAS@cluster0.u0w6u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 
